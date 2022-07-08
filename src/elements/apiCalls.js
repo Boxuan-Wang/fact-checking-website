1d35d071b8468e84495648143ae938752afb4938
Boxuan Wang <boxuan2001@qq.com>
2022-07-08 15:44:31 +0200

    remote connection from local

--- src/elements/apiCalls.js
+++ src/elements/apiCalls.js
@@ -1,4 +1,4 @@
-const sever_url = "http://202.61.203.255:5000";
+const sever_url = "http://localhost:5000";
 /**
  * API call for finding popular checked claims. Send .../popular
  * @returns A list that contains some popular results
